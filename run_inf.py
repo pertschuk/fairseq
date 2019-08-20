8406a6e2afcdb72dee3845e5874d7d1920a9047f
Jack Pertschuk <jp954@cornell.edu>
2019-08-19 20:23:48 -0400

    .

--- run_inf.py
+++ run_inf.py
@@ -1,6 +1,6 @@
 from fairseq.models.roberta import RobertaModel
 
-roberta = RobertaModel.from_pretrained('./checkpoints/', checkpoint_file='checkpoint_best.pt')
+roberta = RobertaModel.from_pretrained('./fever_output/', checkpoint_file='checkpoint_best.pt')
 roberta.eval()  # disable dropout (or leave in train mode to finetune)
 
 tokens = roberta.encode('Roberta is a heavily optimized version of BERT.', 'Roberta is not very optimized.')
