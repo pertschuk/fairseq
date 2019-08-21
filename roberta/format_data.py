278eb67bd92f451530e7ce81bebfc7aab43d4b3d
Jack Pertschuk <jp954@cornell.edu>
2019-08-21 05:31:04 -0400

    .

--- roberta/format_data.py
+++ roberta/format_data.py
@@ -23,9 +23,8 @@ def main(args):
     f2 = open(os.path.join(args.datadir, out_fname + '.label'), 'w+')
     for sample in samples:
       f1.write(sample[0] + '\n')
-      print(sample[1])
-      assert(sample[1] in [0, 1])
-      f2.write(str(sample[1]) + '\n')
+      assert(sample[1] in ['0', '1'])
+      f2.write(sample[1] + '\n')
 
     f1.close()
     f2.close()
