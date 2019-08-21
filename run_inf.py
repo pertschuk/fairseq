82b403e70ae9e6fe1412c2886f4ab6c6e8b20648
Jack Pertschuk <jp954@cornell.edu>
2019-08-21 11:51:34 -0400

    .

--- run_inf.py
+++ run_inf.py
@@ -1,6 +1,9 @@
 from fairseq.models.roberta import RobertaModel
 from fairseq.data.data_utils import collate_tokens
 
+import nltk
+import random
+
 
 MODEL_DIR = './airbnb_training/'
 CHECKPOINT_FILE = 'checkpoint_best.pt'
@@ -13,18 +16,33 @@ class Roberta (object):
     self.model.eval() # disable dropout
     if use_gpu: self.model.cuda()
 
-  def classify_fever(self, claims, evidences):
+  def classify(self, review):
+    reviews = self.batch_review(review)
     roberta = self.model
-    batch = collate_tokens([roberta.encode(c, e) for c, e in zip(claims,evidences)], pad_idx=1)
-    labels = roberta.predict('sentence_classification_head', batch).argmax(dim=1)
-    labels = [CLASSES[label] for label in labels]
-    return labels
+    batch = collate_tokens([roberta.encode(r) for r in reviews], pad_idx=1)
+    label = roberta.predict('sentence_classification_head', batch)
+    return CLASSES[label.sum(dim=0).argmax()]
 
-  def get_embedding(self, sentences, pooling_strategy='cls'):
-    roberta = self.model
-    batch = collate_tokens([roberta.encode(sentence) for sentence in sentences], pad_idx=1)
-    last_layer_features = roberta.extract_features(batch)
-    if pooling_strategy == 'cls':
-      return last_layer_features[0]
-    elif pooling_strategy == 'mean':
-      return last_layer_features.mean(dim=1)
+  def batch_review(self, review):
+    sents = nltk.sent_tokenize(review)
+    buffer = []
+    chunks = []
+    for sent in sents:
+      buffer.append(sent)
+      if (len(buffer)) % 3 == 0:
+        chunks.append(" ".join(buffer))
+        buffer = [buffer[random.randint(0,2)]]
+    if len(buffer) > 1:
+      chunks.append(" ".join(buffer))
+    return chunks
+
+  #
+  # def get_embedding(self, sentences, pooling_strategy='cls'):
+  #   roberta = self.model
+  #   batch = collate_tokens([roberta.encode(sentence) for sentence in sentences], pad_idx=1)
+  #   last_layer_features = roberta.extract_features(batch)
+  #   if pooling_strategy == 'cls':
+  #     return last_layer_features[0]
+  #   elif pooling_strategy == 'mean':
+  #     return last_layer_features.mean(dim=1)
+
