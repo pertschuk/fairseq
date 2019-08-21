a7f7597c2f1f3b2ffd8bcad7d5ff00b126a1cb36
Jack Pertschuk <jp954@cornell.edu>
2019-08-21 12:09:00 -0400

    .

--- run_inf.py
+++ run_inf.py
@@ -29,7 +29,7 @@ class Roberta (object):
     chunks = []
     for sent in sents:
       buffer.append(sent)
-      if (len(buffer)) % 5 == 0:
+      if (len(buffer)) % 3 == 0:
         chunks.append(" ".join(buffer))
         buffer = [buffer[random.randint(0,2)]]
     if len(buffer) > 1:
